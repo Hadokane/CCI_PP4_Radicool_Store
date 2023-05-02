c441e432c3e26dd852dc84fbca8a6006a4df8e31
Hadokane <kanedaveo@gmail.com>
2023-05-02 21:20:59 +0000

    feat(store): add info page for individual products

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = "store"  # links to the namespace specified in radicool/urls
 
 urlpatterns = [
-    path("", views.home, name="home")
+    path("", views.home, name="home"),
+    path("item/<slug:slug>", views.merch_info, name="merch_info"),
 ]
