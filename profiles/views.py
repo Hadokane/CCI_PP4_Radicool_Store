a15c5f5694f8eab7fe5ef071ead362c17cb2e555
Hadokane <kanedaveo@gmail.com>
2023-05-11 20:10:36 +0000

    style(allauth): add CSS styling for django allauth

--- profiles/views.py
+++ profiles/views.py
@@ -12,9 +12,11 @@ from checkout.models import Order
 @login_required
 def profile(request):
     """ Display the user's profile. """
+    profile = get_object_or_404(UserProfile, user=request.user)
 
     template = 'profiles/profile.html'
     context = {
-    }
+        "profile": profile,
+        }
 
     return render(request, template, context)
