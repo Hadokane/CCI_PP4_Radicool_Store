92b18c8c402bdfb78e438e5e63e73d3d86483b96
Hadokane <kanedaveo@gmail.com>
2023-05-17 19:42:50 +0000

    fix(image): make images show on all pages
    Continued fix. Restore images from Bucket to all pages.

--- radicool/settings.py
+++ radicool/settings.py
@@ -29,10 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY", "")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = "DEVELOPMENT" in os.environ
 
-#DEBUG = True  # Delete when done bug fixing.
-
 ALLOWED_HOSTS = ['radicool.herokuapp.com', 'localhost']
-
 
 # Application definition
 
